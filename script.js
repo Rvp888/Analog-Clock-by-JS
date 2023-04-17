4e6848382d0d232a0d403b70407a6ef26bcaefe4
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-17 16:55:53 +0530

    modify file

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 
+const hourHand = document.querySelector('[data-hour-hand]');
+const minuteHand = document.querySelector('[data-minute-hand]');
+const secondHand = document.querySelector('[data-second-hand]');
+
 
