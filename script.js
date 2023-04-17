a33c87c7ba36af882f81e98863c7cf0d5965ae43
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-17 17:09:56 +0530

    modify file

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const minuteHand = document.querySelector('[data-minute-hand]');
 const secondHand = document.querySelector('[data-second-hand]');
 
 
+function setClock() {
+    const currentDate = new Date();
+    const secondsRatio = currentDate.getSeconds() / 60;
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+    console.log("minutesRatio",minutesRatio);
+}
+
+setClock();
