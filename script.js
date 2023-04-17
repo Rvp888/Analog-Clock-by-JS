852e36ad03d0529734137ac583122f9c09ce8c78
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-17 17:23:03 +0530

    modify file

--- script.js
+++ script.js
@@ -8,7 +8,16 @@ function setClock() {
     const currentDate = new Date();
     const secondsRatio = currentDate.getSeconds() / 60;
     const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
-    console.log("minutesRatio",minutesRatio);
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+    setRotation(secondHand, secondsRatio);
+    setRotation(minuteHand, minutesRatio);
+    setRotation(hourHand, hoursRatio);
+}
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360);
 }
 
 setClock();
+
+setInterval(setClock, 1000);
